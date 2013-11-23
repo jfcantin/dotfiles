1e0f7ca36207a4728888c23fdadf02a36f76dd48
Jean-Francois Cantin <jfcantin@jfc-mbpr.home>
2013-11-23 13:38:22 -0700

    Added dependencies file and backup functions
    Backup functions are from gf3/dotfiles

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,18 +1,66 @@
 #!/usr/bin/env bash
-cd "$(dirname "${BASH_SOURCE}")"
-git pull origin master
-function doIt() {
+
+#-----------------------------------------------------------------------------
+# Functions
+#-----------------------------------------------------------------------------
+
+# Notice title
+notice() { echo  "\033[1;32m=> $1\033[0m"; }
+
+backup() {
+  mkdir -p $backupdir
+
+  local files=( $(ls -a) )
+  for file in "${files[@]}"; do
+    in_array $file "${excluded[@]}" || cp -Rf "$HOME/$file" "$backupdir/$file"
+  done
+}
+
+installFiles() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
 		--exclude "README.md" --exclude "LICENSE-MIT.txt" -av --no-perms . ~
-	source ~/.bash_profile
 }
+
+in_array() {
+  local hay needle=$1
+  shift
+  for hay; do
+    [[ $hay == $needle ]] && return 0
+  done
+  return 1
+}
+
+#-----------------------------------------------------------------------------
+# Initialize
+#-----------------------------------------------------------------------------
+
+backupdir="$HOME/.dotfiles-backup/$(date "+%Y%m%d%H%M.%S")"
+excluded=(. .. .git .gitignore bootstrap.sh dependencies.sh Gemfile Gemfile.lock Rakefile README.md)
+
+#-----------------------------------------------------------------------------
+# Install
+#-----------------------------------------------------------------------------
+cd "$(dirname "${BASH_SOURCE}")"
+git pull origin master
+
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt
+  notice "Backup up old files ($backupdir)"
+  backup
+
+	notice "Installing files in home directory"
+	installFiles
 else
 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
 	echo
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt
+  notice "Backup up old files ($backupdir)"
+  backup
+
+	notice "Installing files in home directory"
+	installFiles
 	fi
 fi
-unset doIt
+source ~/.bash_profile
+
+notice "Done"
+exec $SHELL -l
