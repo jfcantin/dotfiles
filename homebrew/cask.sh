a296c994bb119ff05d0e1d80057cee7eff58f37b
Jean-Francois Cantin <jfcantin@gmail.com>
2016-10-03 14:48:49 -0600

    Updated bash and cask.

--- homebrew/cask.sh
+++ homebrew/cask.sh
@@ -2,16 +2,14 @@
 # Install native apps
 brew tap caskroom/cask
 
-function installcask() {
-	brew cask install "${@}" 2> /dev/null
-}
-
-
 brew cask install dropbox 
-brew cask install google-chrome-canary
-#brew cask install iterm2
+brew cask install iterm2
 brew cask install macvim
 brew cask install spectacle
+
+brew tap caskroom/versions
+brew cask install google-chrome-canary
+
 #installcask sublime-text
 #installcask the-unarchiver
 #installcask tor-browser
@@ -21,7 +19,6 @@ brew cask install spectacle
 #installcask vlc
 
 # from caskroom/versions
-#brew tap caskroom/versions
 #installcask google-chrome-canary
 #installcask rstudio-preview
 #installcask sublime-text-3
