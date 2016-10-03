82cd71c777813b845485a171f23e0a27098938cd
Jean-Francois Cantin <jfcantin@gmail.com>
2016-10-03 13:00:04 -0600

    Update bash init

--- npm/modules.sh
+++ npm/modules.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-npm install -g yo
-npm install -g generator-angular
-npm install -g generator-angular
-npm install -g jshint
+# npm install -g yo
+# npm install -g generator-angular
+# npm install -g generator-angular
+# npm install -g jshint
