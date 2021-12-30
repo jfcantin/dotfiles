e305a29f571f0145128844150455a3cad237aa48
Jean-Francois Cantin <jfcantin@gmail.com>
2021-12-30 15:14:00 -0700

    Removed fish and surface dotfiles

--- shell/fish/config.fish
+++
@@ -1,5 +0,0 @@
-for F in $HOME/.config/fish/env/*
-	source $F
-end
-
-set fish_greeting ""
--- shell/fish_functions/posix-source-universal.fish
+++
@@ -1,6 +0,0 @@
-function posix-source-universal
-	for i in (cat $argv)
-		set arr (string split -m1 = $i)
-		set -Ux $arr[1] $arr[2]
-	end
-end
